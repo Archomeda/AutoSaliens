14197c3231268330303896ea732103579913ded1
Archomeda <archomeda@hotmail.com>
2018-06-22 16:01:19 +0200

    Add project files.

---
+++ AutoSaliens/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoSaliens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
