3dcc0cc5ec5bc0066cedf95737169f6caa76f6da
Archomeda <archomeda@hotmail.com>
2018-06-26 01:10:39 +0200

    Fix regex

--- .appveyor/UpdateVersion.ps1
+++ .appveyor/UpdateVersion.ps1
@@ -1,20 +1,20 @@
 Function RegexReplaceFile($file, $id, $replace, $encoding = "ASCII") {
-    $regex = "/\* APPVEYOR_START_$id \*/.*\/* APPVEYOR_END_$id \*/"
+    $regex = "/\* APPVEYOR_START_$id \*/.*/\* APPVEYOR_END_$id \*/"
     (Get-Content $file -encoding $encoding) -replace $regex,$replace | Set-Content $file -encoding $encoding
 }
 
 Function SetVersion() {
-    Write-Host "Set commit version: $env:APPVEYOR_BUILD_VERSION" -ForegroundColor "Yellow"
+    Write-Host "Set version: $env:APPVEYOR_BUILD_VERSION" -ForegroundColor "Yellow"
     RegexReplaceFile -file "AutoSaliens\UpdateChecker.cs" -id "VERSION" -replace $env:APPVEYOR_BUILD_VERSION
 }
 
 Function SetBranch() {
-    Write-Host "Set commit version: $env:APPVEYOR_REPO_BRANCH" -ForegroundColor "Yellow"
+    Write-Host "Set branch: $env:APPVEYOR_REPO_BRANCH" -ForegroundColor "Yellow"
     RegexReplaceFile -file "AutoSaliens\UpdateChecker.cs" -id "BRANCH" -replace $env:APPVEYOR_BUILD_VERSION
 }
 
 Function SetDate() {
-    Write-Host "Set commit date: $env:APPVEYOR_REPO_COMMIT_TIMESTAMP" -ForegroundColor "Yellow"
+    Write-Host "Set date: $env:APPVEYOR_REPO_COMMIT_TIMESTAMP" -ForegroundColor "Yellow"
     RegexReplaceFile -file "AutoSaliens\UpdateChecker.cs" -id "DATE" -replace $env:APPVEYOR_REPO_COMMIT_TIMESTAMP
 }
 
