25739b3e50c181c4584c97e97abb8eec4fe82ef2
Archomeda <archomeda@hotmail.com>
2018-06-29 18:52:02 +0200

    Fix wrong variable name in bot command

--- AutoSaliens/Console/Commands/BotCommand.cs
+++ AutoSaliens/Console/Commands/BotCommand.cs
@@ -13,7 +13,7 @@ namespace AutoSaliens.Console.Commands
             if (string.IsNullOrWhiteSpace(parameters))
             {
                 // Show the current Discord presence setting
-                this.Logger?.LogCommandOutput($"The bot is currently {{value}}{(Program.Settings.EnableDiscordPresence ? "enabled" : "disabled")}{{reset}}.");
+                this.Logger?.LogCommandOutput($"The bot is currently {{value}}{(Program.Settings.EnableBot ? "enabled" : "disabled")}{{reset}}.");
 
                 this.Logger?.LogCommandOutput("You can change the bot by appending either enable or disable to this command: {command}bot {param}<toggle>");
                 this.Logger?.LogCommandOutput("where {param}<toggle>{reset} is replaced with either {value}enable{reset} or {value}disable{reset}.");
@@ -31,7 +31,7 @@ namespace AutoSaliens.Console.Commands
                     return;
                 }
 
-                this.Logger?.LogCommandOutput($"Bot has been {(Program.Settings.EnableDiscordPresence ? "enabled" : "disabled")}.");
+                this.Logger?.LogCommandOutput($"Bot has been {(Program.Settings.EnableBot ? "enabled" : "disabled")}.");
             }
         }
     }
