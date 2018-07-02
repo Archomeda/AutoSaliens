194090a3d184be35dbc2ed699f3dce60962c9017
Archomeda <archomeda@hotmail.com>
2018-07-02 17:44:29 +0200

    Remove unused using

--- AutoSaliens/Api/SaliensApi.cs
+++ AutoSaliens/Api/SaliensApi.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Threading;
 using System.Threading.Tasks;
 using AutoSaliens.Api.Models;
 using Newtonsoft.Json;
